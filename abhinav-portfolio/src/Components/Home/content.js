a1180e30ad2e8dbc759067e1fc98a3de5f5c0517
Abhinav Chinta <abhinavmessi3@gmail.com>
2021-08-05 00:45:04 +0530

    content.js

---
+++ abhinav-portfolio/src/Components/Home/content.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Tilt from "react-parallax-tilt";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+    AiFillInstagram,
+  } from "react-icons/ai";
+  import { FaLinkedinIn } from "react-icons/fa";
+
+  function content() {
+      return(
+          <Container fluid className='home-about-section' id='about'>
+              <Container>
+                  <Row>
+                      
+                  </Row>
+              </Container>
+          </Container>
+      )
+  }
